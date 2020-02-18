25e61b483df58e35e82caa50e3c58e12a3e0cdf9
Amr_Reda <amrreda547@gmail.com>
2020-02-18 17:13:31 +0200

    Update index.vuln.js

--- index.vuln.js
+++ index.vuln.js
@@ -12,5 +12,16 @@ app.post('/test', (req, res) => {
     res.end('finished')
 
 })
+const Vulnerability = (req, res) => {
+    var obj = req.body
 
+    for (let index = 0; index < obj.length; index++) {
+        const element = obj.length[index];
+        
+    }
+    res.end('finished')
+}
+
+
+app.get('/vuln', Vulnerability);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
