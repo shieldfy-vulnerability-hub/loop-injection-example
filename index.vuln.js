a6596674b57d3ecca8b4a7e50d2374bcb53cbc04
amr <amrreda547@gmail.com>
2020-02-23 13:57:49 +0200

    update js vulnerable example

--- index.vuln.js
+++ index.vuln.js
@@ -1,7 +1,24 @@
 var express = require('express');
 var app = express();
 const port = 3000
+const helmet = require('helmet')
 
+var bodyParser = require('body-parser')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(helmet())
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ["'self'", 'maxcdn.bootstrapcdn.com']
+    }
+  }))
+  
 app.post('/test', (req, res) => {
     var obj = req.body
 
