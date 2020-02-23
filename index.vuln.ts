d0876fceb456865a53994fe112cce0b2b47903f8
amr <amrreda547@gmail.com>
2020-02-23 14:22:28 +0200

    disable ts files

--- index.vuln.ts
+++ index.vuln.ts
@@ -1,34 +1,34 @@
-import express from "express";
-import helmet from "helmet";
-import bodyParser from "body-parser";
+// import express from "express";
+// import helmet from "helmet";
+// import bodyParser from "body-parser";
  
-let app = express()
+// let app = express()
 
-app.use(helmet())
-app.use(helmet.contentSecurityPolicy({
-    directives: {
-      defaultSrc: ["'self'"],
-      styleSrc: ["'self'", 'maxcdn.bootstrapcdn.com']
-    }
-  }))
+// app.use(helmet())
+// app.use(helmet.contentSecurityPolicy({
+//     directives: {
+//       defaultSrc: ["'self'"],
+//       styleSrc: ["'self'", 'maxcdn.bootstrapcdn.com']
+//     }
+//   }))
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+// // parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: false }))
  
-// parse application/json
-app.use(bodyParser.json())
+// // parse application/json
+// app.use(bodyParser.json())
 
-const port = 3000
+// const port = 3000
 
-app.post('/test', (req, res) => {
-    var obj = req.body
+// app.post('/test', (req, res) => {
+//     var obj = req.body
 
-    for (let index = 0; index < obj.length; index++) {
-        const element = obj.length[index];
+//     for (let index = 0; index < obj.length; index++) {
+//         const element = obj.length[index];
         
-    }
-    res.end('finished')
+//     }
+//     res.end('finished')
 
-})
+// })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
